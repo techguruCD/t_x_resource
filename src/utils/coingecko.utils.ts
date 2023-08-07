95578d16b383caeb966c5e563c81ba15d35effaf
Richie <molunorichie@gmail.com>
2023-08-07 13:40:34 +0100

    add getCoinTickers - coingecko.utils.ts

--- src/utils/coingecko.utils.ts
+++ src/utils/coingecko.utils.ts
@@ -1,6 +1,13 @@
 import { AxiosInstance, isAxiosError } from "axios";
 import winston from "winston";
 
+interface GetCoinTickerParams {
+    id: string,
+    exchange_ids: string[] | undefined,
+    include_exchange_logo: boolean | undefined,
+    page: number | undefined,
+}
+
 class CoingeckoApi {
     private axios: AxiosInstance;
     private logger: winston.Logger;
@@ -56,23 +63,46 @@ class CoingeckoApi {
         }
     }
 
-    async getCoinInfo(coinId: string) {
+    async getCoinInfo(coin_id: string) {
         const url = `/coins/`
         const params = {
             params: {
-                id: coinId
+                id: coin_id
             }
         }
 
         try {
-            const response = await this.axios.get(url, { params })   
+            const response = await this.axios.get(url, params)
 
-            if (!response.data) {
+            const null_response = !response.data || response.data.length < 1
+            if (null_response) {
                 return undefined
             }
 
-            if (response.data.length < 1) {
-                return undefined;
+            return response.data;
+        } catch (error: any) {
+            this.logError(error, JSON.stringify({ url, params }))
+            return undefined
+        }
+    }
+
+    async getCoinTickers(coin_id: string, exchange_ids?: string[], include_exchange_logo?: boolean, page?: number) {
+        const url = `/coins/${coin_id}/tickers`
+        const params: { params: GetCoinTickerParams } = {
+            params: {
+                id: coin_id,
+                exchange_ids,
+                include_exchange_logo,
+                page: page ?? 100
+            }
+        }
+
+        try {
+            const response = await this.axios.get(url, params)
+
+            const null_response = !response.data || response.data.length < 1
+            if (null_response) {
+                return undefined
             }
 
             return response.data;
